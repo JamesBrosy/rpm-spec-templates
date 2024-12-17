bb044fa947168d308f7e3dcb4357610cfd559612
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2024-12-17 18:53:23 +0800

    Update get-mise-version.sh

--- .github/get-mise-version.sh
+++ .github/get-mise-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/jdx/mise/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")' | tr -d 'v'
+curl -s https://api.github.com/repos/$1/$1/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'
