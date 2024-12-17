1eb76bc40bc31f7484287cfba0e36823ca9573b3
Jo Carllyle <96739684+JamesBrosy@users.noreply.github.com>
2024-12-17 19:30:27 +0800

    Update get-version.sh

--- .github/get-version.sh
+++ .github/get-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/$1/$1/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'
+curl -s https://api.github.com/repos/$1/$2/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'
